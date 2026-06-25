d23a73bf837ae9195f79f4768c7ed2aa9135e41d
Shivansh Shrivastava <shivanshshrivastava2612@gmail.com>
2026-06-25 10:42:07 +0530

    Time: 15 ms (48.42%) | Memory: 48.7 MB (17.25%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
@@ -3,30 +3,31 @@ public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
         long long minimum = 0;
         int i;
+        int n = nums.size();
 
-        vector<vector<long long>> numcost;
+        vector<pair<long long, long long>> numcost;
 
-        for(i=0; i<nums.size(); i++){
+        for(i=0; i<n; i++){
             numcost.push_back({nums[i], cost[i]});
         }
 
         sort(numcost.begin(), numcost.end());
         long long totalweight = 0;
-        for(i=0; i<numcost.size(); i++){
-            totalweight+=numcost[i][1];
+        for(i=0; i<n; i++){
+            totalweight+=numcost[i].second;
         }
         long long curr = 0;
         long long median = 0;
-        for(i=0; i<numcost.size(); i++){
-            curr+=numcost[i][1];
+        for(i=0; i<n; i++){
+            curr+=numcost[i].second;
             if(curr*2>=totalweight){
-                median = numcost[i][0];
+                median = numcost[i].first;
                 break;
             }
         }
 
-        for(i=0; i<numcost.size(); i++){
-            minimum+= abs(numcost[i][0] - median)*(numcost[i][1]);
+        for(i=0; i<n; i++){
+            minimum+= abs(numcost[i].first - median)*(numcost[i].second);
         }
         return minimum;
     }
