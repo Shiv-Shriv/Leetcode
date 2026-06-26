2286b13527ed75af0d8f3a8d1c55cc3dac8bf44a
Shivansh Shrivastava <shivanshshrivastava2612@gmail.com>
2026-06-26 13:20:41 +0530

    Time: 41 ms (90.06%) | Memory: 120 MB (70.36%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -7,7 +7,7 @@ public:
         vector<vector<int>> answer;
         for(int i=0; i<n; i++){
             j=i+2;
-            if(abs(nums[j]-nums[i])<=k&&abs(nums[j]-nums[i+1])<=k&&abs(nums[i]-nums[i+1])<=k){
+            if(abs(nums[j]-nums[i])<=k){
                 answer.push_back({nums[i], nums[i+1], nums[j]});
             }
             else{
