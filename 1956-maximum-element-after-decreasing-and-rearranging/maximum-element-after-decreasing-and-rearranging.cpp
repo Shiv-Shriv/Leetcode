1b11403683b33a652e79b38387f27905b8005840
Shivansh Shrivastava <shivanshshrivastava2612@gmail.com>
2026-06-29 01:59:22 +0530

    Time: 4 ms (88.52%) | Memory: 55.3 MB (36.97%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,12 +4,12 @@ public:
         sort(arr.begin(), arr.end());
         long long i = 0;
         long long n = arr.size();
-        if(arr[0]!=1){
+
             arr[0] = 1;
-        }
+        
 
         while(i<n-1){
-            if(arr[i+1] - arr[i]>1){
+            if(arr[i+1] > 1 + arr[i]){
                 arr[i+1] = arr[i] + 1;
             }
             i++;
